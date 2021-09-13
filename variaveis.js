d6536be249c8d2bed1040f99dcf52aab03d11bd6
Rodrigo Andrade <p.rodrigorosa@gmail.com>
2021-09-12 23:18:23 -0300

    Variáveis

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+//Variáveis e Constantes
+//var
+var a = 3
+//let
+let b = 4
+// Usando var voce pode redeclarar uma variável no mesmo escopo. Mas não é indicado o uso de var se possível use Let.
+var a = 40
+b = 30
+
+console.log(a, b)
+
+a = 400
+b = 500
+
+console.log(a, b)
+//constantes nao podem ser alteradas durante aplicação
+const c = 5
+//c = 50
+console.log(c)
