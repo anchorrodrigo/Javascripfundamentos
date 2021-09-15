e3614c29a2e4b98e5e0c5d37d45c679ac25ac158
Rodrigo Andrade <p.rodrigorosa@gmail.com>
2021-09-14 22:40:04 -0300

    Aulas do dia 13/09/2021

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+// tipo infinity
+console.log(7/0)
+
+console.log("10"/2)
+
+console.log("Show!" * 2)
+//Imprecisão
+console.log(0.1 + 0.7)
+
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
+
---
+++ templateString.js
@@ -0,0 +1,14 @@
+const nome = 'Rebeca'
+const concatenacao ='Olá' + nome + '!'
+//template String
+const template = `
+    olá
+    ${ nome }!`
+    console.log(concatenacao, template)
+
+    //expressoes...
+    console.log(`1+1 = ${1+1}`)
+
+//daqui para Baixo nao entendi bolufas
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}`)
